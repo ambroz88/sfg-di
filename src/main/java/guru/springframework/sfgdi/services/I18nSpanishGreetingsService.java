6a5c3d96229cbae6ada62e753d45001386dfa82c
Jiri Ambroz <ambroz88@seznam.cz>
2020-04-29 14:24:07 +0200

    Add two same services with different profile - EN, ES

---
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingsService.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("ES")
+@Service("I18nService")
+public class I18nSpanishGreetingsService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Hola Mungo - ES";
+    }
+}
